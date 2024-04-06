be0ec05afab549a4d399e4c93906b85f722d391e
Alex Maia <alex.ribeiro.maiaa@gmail.com>
2024-04-05 22:49:38 -0300

    aula js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/* alert("hello world");
+prompt('digite seu nome:');
+confirm('você deseja sair?'); */
+
+// para comentar linhas com atalhos:
+// cntrl + ; (para comentar linha por linha)
+// ou
+// alt + shift + a (para comentar um bloco inteiro)
+
+
+//Entrada
+var nome = prompt('digite seu nome: ')
+var idade = prompt('diga sua idade:') 
+var profissao = prompt('diga sua profissão:')
+//Saida
+//console.log('olá ' + nome + ', seja bem vindo')
+//console.log(`olá ${nome}, seja bem vindo`)
+console.log(`olá ${nome}, você tem ${idade} anos e você trabalha com ${profissao}`)
